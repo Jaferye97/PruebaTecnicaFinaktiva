8d56206c062d05d9317e88fb5f9cc583b9087534
Yeimer Jaramillo <andresjara0897@hotmail.com>
2025-08-15 15:44:25 -0500

    Frontend: Se agrega el componente Sidebar con funcionalidad de navegación.

---
+++ Frontend/src/app/components-general/layout/sidebar/sidebar.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [MatListModule, MatIconModule, RouterModule, CommonModule],
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
+})
+export class SidebarComponent {
+  activeSection: string | null = null;
+
+  menu = [
+    {
+      title: 'Customers',
+      key: 'customers',
+      icon: 'assignment_ind',
+      items: [
+        {
+          title: 'Customer List',
+          key: 'customer-list',
+          route: '',
+        },
+      ],
+    },
+    {
+      title: 'Event Logs',
+      key: 'event-logs',
+      icon: 'assignment',
+      items: [
+        {
+          title: 'Event Logs List',
+          key: 'event-logs-list',
+          route: '',
+        },
+      ],
+    },
+  ];
+
+  toggleSection(key: string) {
+    this.activeSection = this.activeSection === key ? null : key;
+  }
+}
