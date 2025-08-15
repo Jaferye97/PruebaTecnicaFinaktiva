b630bf404703f25da812ab61310093a0831aa356
Yeimer Jaramillo <andresjara0897@hotmail.com>
2025-08-15 15:01:47 -0500

    Backend: Se agrega funcionalidad que, al guardar o actualizar un registro de una entidad, también registra un evento en EventLog.

--- Backend/ReporitorySqlServer/Context/EntityDbContext.cs
+++ Backend/ReporitorySqlServer/Context/EntityDbContext.cs
@@ -13,25 +13,95 @@ namespace ReporitorySqlServer.Context
         : base(options)
         { }
 
-        private void OnBeforeSaving()
+        private void OnBeforeSaving(DateTime localNow)
         {
-            var localNow = DateTime.Now;
-
             foreach (var entry in ChangeTracker.Entries())
             {
+                var keyName = entry.Metadata.FindPrimaryKey()?.Properties.FirstOrDefault()?.Name;
+                var keyValue = keyName != null ? entry.Property(keyName).CurrentValue : Guid.Empty;
+
                 switch (entry.State)
                 {
                     case EntityState.Added:
+                        if(keyValue == null || (Guid)keyValue == Guid.Empty)
+                            entry.Property("Id").CurrentValue = Guid.NewGuid();
+
                         entry.Property("CreatedAt").CurrentValue = localNow;
                         break;
                 }
             }
         }
 
+        private IEnumerable<EventLogsEntity> GetEventLogOnBeforeSaving(DateTime localNow)
+        {
+            var eventLogs = new List<EventLogsEntity>();
+
+            foreach (var entry in ChangeTracker.Entries())
+            {
+                if (entry.Entity is EventLogsEntity)
+                    continue;
+
+                var keyName = entry.Metadata.FindPrimaryKey()?.Properties.FirstOrDefault()?.Name;
+                var keyValue = keyName != null ? entry.Property(keyName).CurrentValue : 0;
+
+                var nameTable = entry.Metadata.GetAnnotations()
+                          .FirstOrDefault(a => a.Name == "Relational:TableName")?.Value?.ToString();
+
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        eventLogs.Add(new EventLogsEntity
+                        {
+                            EventDate = localNow,
+                            Description = $"(INSERT) Nuevo registro en {entry.Entity.GetType().Name}",
+                            ReferenceId = (Guid?)keyValue,
+                            ReferenceEntity = nameTable,
+                            EventType = "API",
+                            CreatedAt = localNow
+                        });
+
+                        break;
+
+                    case EntityState.Modified:
+                        eventLogs.Add(new EventLogsEntity
+                        {
+                            EventDate = localNow,
+                            Description = $"(UPDATE) Actualización en {entry.Entity.GetType().Name}",
+                            ReferenceId = (Guid?)keyValue,
+                            ReferenceEntity = nameTable,
+                            EventType = "API",
+                            CreatedAt = localNow
+                        });
+
+                        break;
+                }
+            }
+
+            return eventLogs;
+        }
+
+        private void OnAfterSaving(DateTime localNow, IEnumerable<EventLogsEntity> eventLogs)
+        {
+            if (eventLogs?.Count() > 0)
+            {
+                EventLog.AddRange(eventLogs);
+                base.SaveChanges();
+            }
+        }
+
         public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
-            OnBeforeSaving();
-            return await base.SaveChangesAsync(cancellationToken);
+            var localNow = DateTime.Now;
+
+            OnBeforeSaving(localNow);
+
+            var eventLogs = GetEventLogOnBeforeSaving(localNow);
+
+            var result = await base.SaveChangesAsync(cancellationToken);
+
+            OnAfterSaving(localNow, eventLogs);
+
+            return result;
         }
 
         public DbSet<CustomersEntity> Customer { get; set; }
