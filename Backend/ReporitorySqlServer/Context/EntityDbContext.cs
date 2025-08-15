fbb8c53a1b970b159df3da05dbea318c92043de5
Yeimer Jaramillo <andresjara0897@hotmail.com>
2025-08-15 01:28:15 -0500

    Backend: Se agrega configuración base de la solución.

---
+++ Backend/ReporitorySqlServer/Context/EntityDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReporitorySqlServer.Context
+{
+    public class EntityDbContext : DbContext
+    {
+        public EntityDbContext()
+        {
+        }
+
+        public EntityDbContext(DbContextOptions<EntityDbContext> options)
+        : base(options)
+        { }
+    }
+}
